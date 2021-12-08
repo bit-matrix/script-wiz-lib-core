7d8fbbc2cd06a54d60fdcc30ee735f76afe6e960
afarukcali <afarukcali@gmail.com>
2021-12-09 00:35:24 +0300

    model folders move from script-wiz-lib

---
+++ src/model/WizDataList.ts
@@ -0,0 +1,13 @@
+import WizData from "@script-wiz/wiz-data";
+import { TxData } from "./TxData";
+
+export type WizDataList = {
+  inputHexes: string[];
+  main: WizData[];
+  alt: WizData[];
+  flow: boolean[];
+  altFlow: boolean[];
+  isStackFailed: boolean;
+  errorMessage?: string;
+  txData?: TxData;
+};
---
+++ src/model/index.ts
@@ -0,0 +1,5 @@
+import { ParseResult, ParseResultData } from "./ParseResult";
+import { WizDataList } from "./WizDataList";
+import { TxData } from "./TxData";
+
+export { WizDataList, ParseResult, ParseResultData, TxData };
