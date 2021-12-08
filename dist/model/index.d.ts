b38782e8e3825f309f23c9d6f7cb30dbbe42bab7
afarukcali <afarukcali@gmail.com>
2021-12-09 00:37:58 +0300

    dist

---
+++ dist/model/index.d.ts
@@ -0,0 +1,4 @@
+import { ParseResult, ParseResultData } from "./ParseResult";
+import { WizDataList } from "./WizDataList";
+import { TxData } from "./TxData";
+export { WizDataList, ParseResult, ParseResultData, TxData };
---
+++ dist/utils/index.d.ts
@@ -0,0 +1,8 @@
+import BN from "bn.js";
+import { WizDataList } from "../model";
+export declare const ZERO_64: BN;
+export declare const MAX_INTEGER_64: BN;
+export declare const MIN_INTEGER_64: BN;
+export declare const NEGATIVE_1_64: BN;
+export declare const currentScope: (wizDataList: WizDataList) => boolean;
+export declare const flipbits: (str: string) => string;
