2a6d62c48b772e3733d8b22298986c34e92ff37c
afarukcali <afarukcali@gmail.com>
2021-12-13 16:40:47 +0300

    Utils functions only for logic functions

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,3 +13,9 @@ export const flipbits = (str: string): string => {
     .map((b: any) => (1 - b).toString())
     .join("");
 };
+
+export const toHexString = (byteArray: Uint8Array) => {
+  return Array.from(byteArray, function (byte) {
+    return ("0" + (byte & 0xff).toString(16)).slice(-2);
+  }).join("");
+};
