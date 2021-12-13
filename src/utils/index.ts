0f3f3d70656bc28e10c8c5f83c8f0c19ad677813
afarukcali <afarukcali@gmail.com>
2021-12-13 16:14:41 +0300

    utils functions fixed.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,14 +1,11 @@
 import WizData from "@script-wiz/wiz-data";
 import BN from "bn.js";
 import { convert64 } from "../conversion";
-import { WizDataList } from "../model";
 
 export const ZERO_64 = new BN(convert64(WizData.fromHex("00")).bin, 2);
 export const MAX_INTEGER_64 = new BN("1111111111111111111111111111111111111111111111111111111101111111", 2);
 export const MIN_INTEGER_64 = new BN("0000000000000000000000000000000000000000000000000000000010000000", 2);
 export const NEGATIVE_1_64 = new BN("0000000000000000000000000000000000000000000000000000000010000001", 2);
-
-export const currentScope = (wizDataList: WizDataList): boolean => wizDataList.flow[wizDataList.flow.length - 1];
 
 export const flipbits = (str: string): string => {
   return str
