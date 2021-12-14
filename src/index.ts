5d869ec6db3089ae543ae8f40afcf46cd582a554
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-14 23:28:42 +0300

    TAPROOT_VERSION export

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,6 @@ import * as locktime from "./locktime";
 import * as splices from "./splices";
 import * as stacks from "./stacks";
 import * as taproot from "./taproot";
-import { TxData, TxInput, TxOutput } from "./model";
+import { TxData, TxInput, TxOutput, TAPROOT_VERSION } from "./model";
 
-export { arithmetics, bitwise, conversion, arithmetics64, crypto, introspection, locktime, splices, stacks, taproot, TxData, TxInput, TxOutput };
+export { arithmetics, bitwise, conversion, arithmetics64, crypto, introspection, locktime, splices, stacks, taproot, TxData, TxInput, TxOutput, TAPROOT_VERSION };
