363dfc9a9186975d1765f48532806faa239cac19
huseyinsaritas <hsynsrtss@gmail.com>
2021-12-14 23:28:48 +0300

    dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -8,5 +8,5 @@ import * as locktime from "./locktime";
 import * as splices from "./splices";
 import * as stacks from "./stacks";
 import * as taproot from "./taproot";
-import { TxData, TxInput, TxOutput } from "./model";
-export { arithmetics, bitwise, conversion, arithmetics64, crypto, introspection, locktime, splices, stacks, taproot, TxData, TxInput, TxOutput };
+import { TxData, TxInput, TxOutput, TAPROOT_VERSION } from "./model";
+export { arithmetics, bitwise, conversion, arithmetics64, crypto, introspection, locktime, splices, stacks, taproot, TxData, TxInput, TxOutput, TAPROOT_VERSION };
