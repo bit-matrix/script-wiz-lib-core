c1cec836f3b018fb260701308121bb97c883aad6
afarukcali <afarukcali@gmail.com>
2021-12-17 23:03:30 +0300

    dist

--- dist/crypto.d.ts
+++ dist/crypto.d.ts
@@ -16,10 +16,14 @@ declare type Keys = {
     publicKey: WizData;
     uncompressedPubKey: WizData;
 };
+declare type Signs = {
+    sign: WizData;
+    derEncodedSign: WizData;
+};
 export declare const secp256k1KeyGenerator: () => Keys;
 export declare const schnorrKeyGenerator: () => Keys;
-export declare const secp256k1Sign: (message: WizData, privateKey: WizData) => WizData;
-export declare const schnorrSign: (message: WizData, privateKey: WizData) => WizData;
+export declare const secp256k1Sign: (message: WizData, privateKey: WizData) => Signs;
+export declare const schnorrSign: (message: WizData, privateKey: WizData) => Signs;
 export declare const secp256k1Verify: (message: WizData, signature: WizData, publicKey: WizData) => WizData;
 export declare const secp256k1CreatePublicKey: (privateKey: WizData) => Keys;
 export declare const schnorrCreatePublicKey: (privateKey: WizData) => Keys;
