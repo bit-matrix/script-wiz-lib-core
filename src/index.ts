a98363f1bc65f7d48bd441ffc79d437fb60cc17a
afarukcali <afarukcali@gmail.com>
2022-02-18 15:24:54 +0300

    address export.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import * as address from "./addresses";
 import * as arithmetics from "./arithmetics";
 import * as bitwise from "./bitwise";
 import * as convertion from "./convertion";
@@ -10,4 +11,4 @@ import * as stacks from "./stacks";
 import * as taproot from "./taproot";
 import { TxData, TxInput, TxOutput, TAPROOT_VERSION } from "./model";
 
-export { arithmetics, bitwise, convertion, arithmetics64, crypto, introspection, locktime, splices, stacks, taproot, TxData, TxInput, TxOutput, TAPROOT_VERSION };
+export { address, arithmetics, bitwise, convertion, arithmetics64, crypto, introspection, locktime, splices, stacks, taproot, TxData, TxInput, TxOutput, TAPROOT_VERSION };
