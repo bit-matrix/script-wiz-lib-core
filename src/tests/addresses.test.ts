3fe76c17c6439edd2df12589f5113dc463325be3
afarukcali <afarukcali@gmail.com>
2022-02-18 15:23:37 +0300

    test fixed

--- src/tests/addresses.test.ts
+++ src/tests/addresses.test.ts
@@ -2,9 +2,9 @@ import WizData from "@script-wiz/wiz-data";
 import { createBech32Address } from "../addresses";
 
 test("bech32test ", () => {
-  const data = WizData.fromNumber(1);
+  const data = WizData.fromHex("0100");
 
-  const res = createBech32Address("tex", data);
+  const res = createBech32Address(data, "bc", 0);
 
   console.log(res);
 });
