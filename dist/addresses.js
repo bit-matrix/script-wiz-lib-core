7cfa979a4f6755952ad28b5d938f4bd400ac9226
afarukcali <afarukcali@gmail.com>
2022-02-20 16:37:56 +0300

    dist.

--- dist/addresses.js
+++ dist/addresses.js
@@ -2,8 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createBech32Address = void 0;
 var bech32_1 = require("bech32");
+var crypto_1 = require("./crypto");
+// version 0 -> segwit
+// version 1 -> taproot
+// prefix bc -> bitcoin mainnet
+// prefix tb -> bitcoin testnet
 var createBech32Address = function (data, prefix, version) {
-    var words = bech32_1.bech32.toWords(Buffer.from(data.hex, "hex"));
+    var hashedData = (0, crypto_1.sha256)(data).toString();
+    var words = bech32_1.bech32.toWords(Buffer.from(hashedData, "hex"));
     words.unshift(version);
     var address = version === 0 ? bech32_1.bech32.encode(prefix, words) : bech32_1.bech32m.encode(prefix, words);
     return address;
