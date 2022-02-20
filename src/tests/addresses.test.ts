9644966ce30ad67cede9c2d28545882eb977dad3
afarukcali <afarukcali@gmail.com>
2022-02-20 16:37:36 +0300

    createBech32Address witness data hash.

--- src/tests/addresses.test.ts
+++ src/tests/addresses.test.ts
@@ -2,7 +2,7 @@ import WizData from "@script-wiz/wiz-data";
 import { createBech32Address } from "../addresses";
 
 test("bech32test ", () => {
-  const data = WizData.fromHex("0100");
+  const data = WizData.fromHex("1863143c14c5166804bd19203356da136c985678cd4d27a1b8c6329604903262");
 
   const res = createBech32Address(data, "bc", 0);
 
