568c1087b0cfd7c0003758e738e641348a844916
ayseren <ayse.eren@windowslive.com>
2022-02-25 16:06:28 +0300

    serialization: segwitSerialization validations were added.

--- src/serialization.ts
+++ src/serialization.ts
@@ -20,25 +20,40 @@ import { size } from "./splices";
 
 export const segwitSerialization = (data: TxData) => {
   const currentInput = data.inputs[data.currentInputIndex];
+
   const scriptCode = WizData.fromHex(currentInput.scriptPubKey);
+  if (currentInput.scriptPubKey === "") throw "scriptPubkey must not be empty in transaction template";
+
   const vout = numToLE32(WizData.fromNumber(Number(currentInput.vout))).hex;
+  if (currentInput.vout === "") throw "Vout must not be empty in transaction template";
+
   const inputAmount = numToLE64(WizData.fromNumber(Number(currentInput.amount) * 100000000)).hex;
+  if (currentInput.amount === "") throw "Amount must not be empty in transaction template";
+
   const timelock = numToLE32(WizData.fromNumber(Number(data.timelock))).hex;
+  if (data.timelock === "") throw "Timelock must not be empty in transaction template";
+
   const version = numToLE32(WizData.fromNumber(Number(data.version))).hex;
+  if (data.version === "") throw "Version must not be empty in transaction template";
 
   // 2 (32-byte hash)
   const hashPrevouts = hash256(WizData.fromHex(hexLE(currentInput.previousTxId) + vout)).toString();
+  if (currentInput.previousTxId === "") throw "Previous TX ID must not be empty in transaction template";
 
   const nsequence = hexLE(currentInput.sequence);
+  if (currentInput.sequence === "") throw "Sequence must not be empty in transaction template";
 
   // 3 (32-byte hash)
   const hashSequence = hash256(WizData.fromHex(nsequence)).toString();
+  if (hashSequence === "") throw "Sequence must not be empty in transaction template";
 
   // 4. outpoint (32-byte hash + 4-byte little endian)
   const outpoint = hexLE(currentInput.previousTxId) + vout;
+  if (outpoint === "") throw "Previous TX ID and Vout must not be empty in transaction template";
 
   // 5. script code hash
   const scriptCodeSize = size(scriptCode).hex.substring(0, 2);
+  if (scriptCodeSize === "") throw "scriptPubkey must not be empty in transaction template";
 
   // 8 hashOutputs
   const hashOutputs = calculateHashOutputs(data.outputs);
@@ -50,6 +65,8 @@ const calculateHashOutputs = (outputs: TxOutput[]) => {
   let hashOutputs = "";
 
   outputs.forEach((output: TxOutput) => {
+    if (output.amount === "" || output.scriptPubKey === "") throw "Amount and scriptPubkey must not be empty in output transaction template";
+
     hashOutputs += numToLE64(WizData.fromNumber(Number(output.amount) * 100000000)).hex + size(WizData.fromHex(output.scriptPubKey)).hex + output.scriptPubKey;
   });
 
