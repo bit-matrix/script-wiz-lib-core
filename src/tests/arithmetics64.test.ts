4c23af6a699ddd4065fa81415663f86d3edd5848
afarukcali <afarukcali@gmail.com>
2022-04-18 15:04:43 +0300

    MIN_INTEGER_64 value fixed.

--- src/tests/arithmetics64.test.ts
+++ src/tests/arithmetics64.test.ts
@@ -1,12 +1,12 @@
 // import { numberTestData } from "./data/number";
 import WizData from "@script-wiz/wiz-data";
-import { add64 } from "../arithmetics64";
+import { add64, sub64 } from "../arithmetics64";
 
 test("add 64 function test", () => {
-  const input1 = WizData.fromHex("f4ffffffffffffff");
-  const input2 = WizData.fromHex("3100000000000000");
+  const input1 = WizData.fromHex("0094357700000000");
+  const input2 = WizData.fromHex("ce93357700000000");
 
-  const result = add64(input1, input2);
+  const result = sub64(input1, input2);
 
-  console.log(result);
+  // console.log(result);
 });
