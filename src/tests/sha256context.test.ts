2358c7f8e8b2d3f5cf9b38489b617c8b821c9f4b
afarukcali <afarukcali@gmail.com>
2022-08-18 14:01:08 +0300

    @bitmatrix/sha256streaming version updated.

--- src/tests/sha256context.test.ts
+++ src/tests/sha256context.test.ts
@@ -1,7 +1,7 @@
-import { sha256context } from "@bitmatrix/sha256streaming";
+import Sha256Stream from "@bitmatrix/sha256streaming";
 
 test("sha256 context test", () => {
-  const result = sha256context("aabbcc");
+  const result = Sha256Stream.sha256Initializer("aabbcc");
 
   console.log(result);
 });
