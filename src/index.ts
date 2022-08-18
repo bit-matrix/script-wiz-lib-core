6195cbb0851f40b82705a5afbc8fdadf42926e3c
afarukcali <afarukcali@gmail.com>
2022-08-18 14:05:07 +0300

    OP_SHA256INITIALIZE , OP_SHA256UPDATE and OP_SHA256FINALIZE op codes logics added.

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import * as introspection from "./introspection";
 import * as locktime from "./locktime";
 import * as splices from "./splices";
 import * as stacks from "./stacks";
+import * as streaming from "./streaming";
 import * as sha256d from "./sha256d";
 import * as taproot from "./taproot";
 import { TxData, TxInput, TxOutput, TAPROOT_VERSION } from "./model";
@@ -24,8 +25,9 @@ export {
   locktime,
   splices,
   stacks,
-  taproot,
+  streaming,
   sha256d,
+  taproot,
   utils,
   TxData,
   TxInput,
